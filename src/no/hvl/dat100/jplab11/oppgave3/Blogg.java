9d6534610e70490796aaac48723faddb4b0435e3
Krist <Krist@DESKTOP-012IUBU>
2022-11-10 10:27:10 +0100

    fikser ingenting

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -5,46 +5,66 @@ import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
+	private Innlegg[] innleggtabell;
+	private int nesteledige;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggtabell = new Innlegg[20];
+		this.nesteledige = 0;
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggtabell = new Innlegg[lengde];
+		this.nesteledige = 0;
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return innleggtabell.length;
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		return innleggtabell;
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		for(int i = 0; i < innleggtabell.length; i++) {
+			if( innlegg.erLik(innleggtabell [i])){
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+			if (finnInnlegg(innlegg) == -1) {
+				return false;
+			}
+			return true;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+			if (nesteledige < innleggtabell.length) {
+				return true;
+			}
+			return false;
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+			if (ledigPlass()) {
+				this.innleggtabell[nesteledige] = innlegg;
+				nesteledige ++;
+				return true;
+			}
+			return false;
 	}
 	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		String output = "";
+		output += innleggtabell.length +"\n";
+		for(Innlegg innlegg : innleggtabell) {
+			output += innlegg.toString();
+		}
+		return output;
 	}
 
 	// valgfrie oppgaver nedenfor
